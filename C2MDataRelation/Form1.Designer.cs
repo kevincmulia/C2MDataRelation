c9f056bdf96b67f235f240259b3279e16b93eed8
jpitoby <jpitoby@akroconsulting.com>
2023-06-08 22:12:30 +0700

    fix bug

--- C2MDataRelation/Form1.Designer.cs
+++ C2MDataRelation/Form1.Designer.cs
@@ -60,7 +60,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 40);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Send";
+            this.button1.Text = "Open";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
